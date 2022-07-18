de5d5bdb75fa788735eaca13481067759c34f3b8
Jimmy Zhang <zhang.arthur@gmail.com>
2022-07-18 20:20:40 +0800

    ApplicationInsights-dotnet-webapp demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using videowebapp.Models;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace videowebapp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly TelemetryClient _telemetryClient;
+    private readonly TelemetryConfiguration _telemetryConfiguration;
+
+    
+    public HomeController(ILogger<HomeController> logger,TelemetryClient telemetryClient,TelemetryConfiguration telemetryConfiguration)
+    {
+        _logger = logger;
+        _telemetryClient = telemetryClient;
+        _telemetryConfiguration = telemetryConfiguration;
+
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Like(string button)
+    {
+        ViewBag.Message = "Thank you for your response";
+        _telemetryClient.TrackEvent(eventName: "LikeClicked");
+        _telemetryClient.TrackEvent(eventName: "CommentSubmitted");
+        _telemetryClient.TrackEvent("VideoUploaded", new Dictionary<string, string> {{"Category", "Sports"}, {"Format", "mp4"}});
+        Metric userResponse = _telemetryClient.GetMetric("UserResponses", "Kind");
+
+        userResponse.TrackValue(24, "Likes");
+        userResponse.TrackValue(5, "Loves");
+        return View("Index");
+       
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ApplicationInsights-dotnet-webapp
+Learning objectives In this module, you will:
+
+* Learn the benefits of adding the Application Insights SDK to the source code of an Azure App Service web app
+* Install the Application Insights SDK in an ASP.NET Core web app
+* Instrument a web app with code to gather information about custom events
